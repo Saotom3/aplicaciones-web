98a127f40bf573fd59343edc7a9508857a2bd66b
Marko <136510400+Saotom3@users.noreply.github.com>
2024-11-06 21:13:22 -0500

    Update enunciado03.dart

--- tareas/semana4/enunciado03.dart
+++ tareas/semana4/enunciado03.dart
@@ -1,3 +1,19 @@
+/*
+Los cálculos salariales de los vendedores de una empresa se calculan de la 
+siguiente manera: 
+• Sueldo básico: S/.600 
+• Comisión: 7% del importe total vendido si es que el importe total vendido es 
+mayor a S/.15000; en caso contrario, 5% del importe total vendido 
+• Bonificación: S/.25 por cada hijo si es que el número de hijos es menor a 5; 
+en caso contrario, S/.22 por cada hijo 
+• Sueldo bruto: La suma del sueldo básico, más la comisión y más la bonificación 
+• Descuento: 15% del sueldo bruto si es que el sueldo bruto es mayor que S/.3500; 
+en caso contrario, 11% del sueldo bruto 
+• Sueldo neto: La resta del sueldo bruto menos el descuento 
+Dado el importe total vendido y el número de hijos de un vendedor, diseñe un programa 
+que determine el sueldo básico, la comisión, la bonificación, el sueldo bruto, el 
+descuento y el sueldo neto.
+*/
 import 'dart:io';
 
 void main() {
