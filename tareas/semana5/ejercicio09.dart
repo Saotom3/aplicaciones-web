c43227f79263fae715bfeb5a78ebeeea1546ecd7
Marko <136510400+Saotom3@users.noreply.github.com>
2024-11-06 21:23:06 -0500

    Update ejercicio09.dart

--- tareas/semana5/ejercicio09.dart
+++ tareas/semana5/ejercicio09.dart
@@ -1,3 +1,10 @@
+/*
+Ejercicio 9: Cálculo de potencias usando multiplicación repetida
+Crea un programa que calcule la potencia de un número usando multiplicación 
+repetida, es decir, sin utilizar la función Math.pow(). El programa debe 
+solicitar una base y un exponente, y luego calcular la potencia utilizando 
+un bucle while o for.
+*/
 import 'dart:io';
 
 void main() {
