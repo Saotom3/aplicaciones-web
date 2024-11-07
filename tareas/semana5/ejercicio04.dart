c12fe15ec9209b9636be706d240340dc9b40582b
Marko <136510400+Saotom3@users.noreply.github.com>
2024-11-06 21:18:35 -0500

    Update ejercicio04.dart

--- tareas/semana5/ejercicio04.dart
+++ tareas/semana5/ejercicio04.dart
@@ -1,3 +1,8 @@
+/*
+Ejercicio 4: Inversión de un número
+Crea un programa que invierta los dígitos de un número entero ingresado por el 
+usuario, utilizando un bucle while para extraer y reordenar los dígitos.
+*/
 import 'dart:io';
 
 void main() {
