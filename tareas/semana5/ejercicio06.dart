fa612cd39dd13648d91f7494e2d3b05b85e4636e
Marko <136510400+Saotom3@users.noreply.github.com>
2024-11-06 21:20:20 -0500

    Update ejercicio06.dart

--- tareas/semana5/ejercicio06.dart
+++ tareas/semana5/ejercicio06.dart
@@ -1,3 +1,10 @@
+/*
+Ejercicio 6: Número perfecto
+Implementa un programa que encuentre y muestre todos los números perfectos 
+entre 1y 10,000. Un número perfecto es aquel que es igual a la suma de sus
+divisores propios. Usa un bucle para iterar y otro para encontrar los divisores 
+de cada número.
+*/
 void main() {
   print("Números perfectos entre 1 y 10,000:");
 
